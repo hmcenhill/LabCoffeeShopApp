223f814ad1e67c93b95fce8590c1409a6ed90ff8
Hermann I McEnhill <hmcenhill@gmail.com>
2019-07-25 22:05:01 -0400

    Initial code commit. User MVC, Product M

---
+++ LabCoffeeShopApp/Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabCoffeeShopApp.Data
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public float Quantity { get; set; }
+        public float Price { get; set; }
+    }
+}
